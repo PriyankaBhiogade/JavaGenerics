42590063a2ac8e335354447eb6c146409e532942
PriyankaBhiogade <bhiogadepriyanka93@gmail.com>
2019-11-28 10:30:13 +0530

    refactor remove repltation

--- src/main/java/com/generic/ArrayExample.java
+++ src/main/java/com/generic/ArrayExample.java
@@ -2,30 +2,23 @@ package com.generic;
 
 public class ArrayExample {
     public static void main(String[] args) {
-        int num[]={1,2,3,4,5};
-        double  doubleArray[]={1.5,2.5,3.5,4.5,5.5};
-        char name[]={'a','b','c','d','e'};
-        toPrint(num);
+        Integer[] intArray = {1, 2, 3, 4, 5};
+        Double[] doubleArray = {1.5, 2.5, 3.5, 4.5, 5.5};
+        Character[] charArray = {'a', 'b', 'c', 'd', 'e'};
+        toPrint(intArray);
         toPrint(doubleArray);
-        toPrint(name);
+        toPrint(charArray);
     }
 
-    private static void toPrint(char[] name) {
-        for (int i=0;i<name.length;i++){
-            System.out.println(name[i]);
+    private static <E> void toPrint(E[] intArray) {
+        for (E element : intArray) {
+            System.out.println(element);
         }
     }
-
-    private static void toPrint(double[] doubleArray) {
-        for(int i=0;i<doubleArray.length;i++){
-            System.out.println("DoubleArray::"+i);
-        }
-    }
-
-    private static void toPrint(int[] num) {
-        for(int i=0;i<num.length;i++){
-            System.out.println("IntArray::"+i);
-        }
-    }
-
 }
+
+
+
+
+
+
